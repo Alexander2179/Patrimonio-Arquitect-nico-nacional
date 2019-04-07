3cf667f58da89342c2cf8a31bdfe9c7cce75d557
estebanrl21 <43360803+estebanrl21@users.noreply.github.com>
2019-04-07 11:55:08 -0600

    Orden
    Estructura carpetas, paginas individuales,  carpeta de Proyecto, carpeta requerimiento proyecto.

---
+++ Proyecto/alexLimon.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Alajuela</title>
+    <?php include("header.php")?>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Proyecto/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Header</title>
+     <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/full-width-pics.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Patrimonios Arquitectonicos</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Inicio
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">San Jose</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Alajuela</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Cartago</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Heredia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Guanacaste</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Puntarenas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Limón</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="py-5 bg-image-full" style="background-image: url('https://unsplash.it/1900/1080?image=1076');">
+        <img class="img-fluid d-block mx-auto" src="http://placehold.it/200x200&text=Logo" alt="">
+    </header>
+
+</body>
+
+</html>
