63470629b94480175d33e1cadecd79c99888cb4c
Alexander2179 <43360819+Alexander2179@users.noreply.github.com>
2019-04-15 10:39:47 -0600

    guanacaste
    se agrega fotos de liberia

---
+++ Proyecto/liberia.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+		<meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+        <title>liberia_Guanacaste</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <meta name="description" content="Thumbnail Proximity Effect with jQuery and CSS3" />
+        <meta name="keywords" content="thumbnails, jquery, proximity, effect, css3, scale, mouse, hover" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="http://localhost/Patrimonio-Arquitectnico-nacional/Proyecto/css/style4.css" />
+        
+    </head>
+    <body id="page">
+        <div class="container">
+			
+            
+			<header>
+				<h1>liberia <span>Costa Rica</span> guanacaste</h1>
+				
+				
+			</header>
+			
+			<section class="pe-container">
+				<ul id="pe-thumbs" class="pe-thumbs">
+					<li><a href="#"><img src="img/guanacaste/liberia/1Antigua%20Casona%20Hacienda%20San%20Luis.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/2Antigua%20Escuela%20Rafael%20Yglesias%20Castro.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/3Casa%20Baltodano%20Brice%C3%B1o.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/4Casa%20de%20la%20Cultura.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/5Casa%20de%20la%20Cultura2.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/6Casa%20Granja%20D%C3%A1vila.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/7Casa%20Z%C3%BA%C3%B1iga%20Clachar.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/8Casona%20Hacienda%20Las%20%C3%81nimas..jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+					<li><a href="#"><img src="img/guanacaste/liberia/9Casona%20Hacienda%20Santa%20Rosa.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/10Cuartel.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/11Ecomuseo%20de%20las%20Minas%20de%20Abangares.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>                                   
+                    <li><a href="#"><img src="img/guanacaste/liberia/12emplo%20Cat%C3%B3lico%20El%20Se%C3%B1or%20de%20La%20Agon%C3%ADa%20(Ermita).jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>                   
+                    <li><a href="#"><img src="img/guanacaste/liberia/13Escuela%20Ascenci%C3%B3n%20Esquivel%20Ibarra.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>					
+                    <li><a href="#"><img src="img/guanacaste/liberia/14Escuela%20Luis%20L%C3%A9ipold%20Herberth.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/15Gobernaci%C3%B3n.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/16Monumento%20Nacional%20El%20Farall%C3%B3n.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/17Puente%20Real.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/18Quiosco%20del%20Parque%20Mario%20Ca%C3%B1as%20Ruiz.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+                    <li><a href="#"><img src="img/guanacaste/liberia/19Vivienda%20Esperanza%20Castrillo%20Rovia.jpg" /><div class="pe-description"><h3>time</h3><p>Since time, and his predestinated end</p></div></a></li>
+										
+					
+
+					
+					
+				</ul>
+			</section>
+        </div>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.proximity.js"></script>
+		<script type="text/javascript">
+			$(function() {
+				
+				var Photo	= (function() {
+					
+						// list of thumbs
+					var $list		= $('#pe-thumbs'),
+						// list's width and offset left.
+						// this will be used to know the position of the description container
+						listW		= $list.width(),
+						listL		= $list.offset().left,
+						// the images
+						$elems		= $list.find('img'),
+						// the description containers
+						$descrp		= $list.find('div.pe-description'),
+						// maxScale : maximum scale value the image will have
+						// minOpacity / maxOpacity : minimum (set in the CSS) and maximum values for the image's opacity
+						settings	= {
+							maxScale	: 1.3,
+							maxOpacity	: 0.9,
+							minOpacity	: Number( $elems.css('opacity') )
+						},
+						init		= function() {
+							
+							// minScale will be set in the CSS
+							settings.minScale = _getScaleVal() || 1;
+							// preload the images (thumbs)
+							_loadImages( function() {
+								
+								_calcDescrp();
+								_initEvents();
+							
+							});
+						
+						},
+						// Get Value of CSS Scale through JavaScript:
+						// http://css-tricks.com/get-value-of-css-rotation-through-javascript/
+						_getScaleVal= function() {
+						
+							var st = window.getComputedStyle($elems.get(0), null),
+								tr = st.getPropertyValue("-webkit-transform") ||
+									 st.getPropertyValue("-moz-transform") ||
+									 st.getPropertyValue("-ms-transform") ||
+									 st.getPropertyValue("-o-transform") ||
+									 st.getPropertyValue("transform") ||
+									 "fail...";
+
+							if( tr !== 'none' ) {	 
+
+								var values = tr.split('(')[1].split(')')[0].split(','),
+									a = values[0],
+									b = values[1],
+									c = values[2],
+									d = values[3];
+
+								return Math.sqrt( a * a + b * b );
+							
+							}
+							
+						},
+						// calculates the style values for the description containers,
+						// based on the settings variable
+						_calcDescrp	= function() {
+							
+							$descrp.each( function(i) {
+							
+								var $el		= $(this),
+									$img	= $el.prev(),
+									img_w	= $img.width(),
+									img_h	= $img.height(),
+									img_n_w	= settings.maxScale * img_w,
+									img_n_h	= settings.maxScale * img_h,
+									space_t = ( img_n_h - img_h ) / 2,
+									space_l = ( img_n_w - img_w ) / 2;
+								
+								$el.data( 'space_l', space_l ).css({
+									height	: settings.maxScale * $el.height(),
+									top		: -space_t,
+									left	: img_n_w - space_l
+								});
+							
+							});
+						
+						},
+						_initEvents	= function() {
+						
+							$elems.on('proximity.Photo', { max: 80, throttle: 10, fireOutOfBounds : true }, function(event, proximity, distance) {
+								
+								var $el			= $(this),
+									$li			= $el.closest('li'),
+									$desc		= $el.next(),
+									scaleVal	= proximity * ( settings.maxScale - settings.minScale ) + settings.minScale,
+									scaleExp	= 'scale(' + scaleVal + ')';
+								
+								// change the z-index of the element once it reaches the maximum scale value
+								// also, show the description container
+								if( scaleVal === settings.maxScale ) {
+									
+									$li.css( 'z-index', 1000 );
+									
+									if( $desc.offset().left + $desc.width() > listL + listW ) {
+										
+										$desc.css( 'left', -$desc.width() - $desc.data( 'space_l' ) );
+									
+									}
+									
+									$desc.fadeIn( 800 );
+									
+								}	
+								else {
+									
+									$li.css( 'z-index', 1 );
+								
+									$desc.stop(true,true).hide();
+								
+								}	
+								
+								$el.css({
+									'-webkit-transform'	: scaleExp,
+									'-moz-transform'	: scaleExp,
+									'-o-transform'		: scaleExp,
+									'-ms-transform'		: scaleExp,
+									'transform'			: scaleExp,
+									'opacity'			: ( proximity * ( settings.maxOpacity - settings.minOpacity ) + settings.minOpacity )
+								});
+
+							});
+						
+						},
+						_loadImages	= function( callback ) {
+							
+							var loaded 	= 0,
+								total	= $elems.length;
+							
+							$elems.each( function(i) {
+								
+								var $el = $(this);
+								
+								$('<img/>').load( function() {
+									
+									++loaded;
+									if( loaded === total )
+										callback.call();
+									
+								}).attr( 'src', $el.attr('src') );
+							
+							});
+						
+						};
+					
+					return {
+						init	: init
+					};
+				
+				})();
+				
+				Photo.init();
+				
+			});
+		</script>
+    </body>
+</html>
